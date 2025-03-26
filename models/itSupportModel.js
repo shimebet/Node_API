600a57f5ffb7e6f6415ad07ac853e5face8fc2bd
Shime <152897503+ShimelisTechane@users.noreply.github.com>
2025-03-27 00:27:33 +0300

    modify user referes

--- models/itSupportModel.js
+++ models/itSupportModel.js
@@ -11,7 +11,7 @@ const itsupportSchema = new mongoose.Schema(
 
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: 'users',
       required: true,
     },
   },
