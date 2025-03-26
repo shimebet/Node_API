33ddeac85f73866df1d1018720f45ed8bacd2869
Shime <152897503+ShimelisTechane@users.noreply.github.com>
2025-03-27 00:30:20 +0300

    modify user referes2

--- routes/supportRoutes.js
+++ routes/supportRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Support = require('../models/itSupportModel');
-const User = require('../models/users'); // 👈 import your User model
+const User = require('../models/User'); // 👈 import your User model
 const { protect } = require('../middleware/authMiddleware');
 
 // GET all support issues with user info
